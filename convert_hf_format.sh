4b4f67767b17ed5e0dc8d5611a99ca142fad239a
7k7k <wuqikai03@kuaishou.com>
2025-07-02 04:14:52 +0800

    57k

--- convert_hf_format.sh
+++ convert_hf_format.sh
@@ -1,11 +1,3 @@
-MODEL_DIR="/path/to/IndexTeam/Index-TTS"
-VLLM_DIR="$MODEL_DIR/vllm"
-
-mkdir -p "$VLLM_DIR"
-
-wget https://modelscope.cn/models/openai-community/gpt2/resolve/master/tokenizer.json -O "$VLLM_DIR/tokenizer.json"
-wget https://modelscope.cn/models/openai-community/gpt2/resolve/master/tokenizer_config.json -O "$VLLM_DIR/tokenizer_config.json"
-
 python convert_hf_format.py --model_dir "$MODEL_DIR"
 
 echo "All operations completed successfully!"
