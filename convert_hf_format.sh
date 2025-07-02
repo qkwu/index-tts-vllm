6dbabb898bd2c8c8294c34382aecec2b670f208a
7k7k <wuqikai03@kuaishou.com>
2025-07-02 14:23:23 +0800

    57k

--- convert_hf_format.sh
+++ convert_hf_format.sh
@@ -1,3 +1,5 @@
+MODEL_DIR="/home/code/index-tts/checkpoints/Index-TTS"
+VLLM_DIR="$MODEL_DIR/vllm"
 python convert_hf_format.py --model_dir "$MODEL_DIR"
 
 echo "All operations completed successfully!"
