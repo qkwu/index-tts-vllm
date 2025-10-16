1e90340408fce0e3cfd921c5a3d1c3ce488e4eeb
7k7k <wuqikai03@kuaishou.com>
2025-10-16 11:40:37 +0800

    index-tts-vllm

--- indextts/infer_vllm_v2.py
+++ indextts/infer_vllm_v2.py
@@ -95,6 +95,13 @@ class IndexTTS2:
         self.gpt_path = os.path.join(self.model_dir, self.cfg.gpt_checkpoint)
         load_checkpoint(self.gpt, self.gpt_path)
         self.gpt = self.gpt.to(self.device)
+
+        # 强制设置 max_tokens，解决 1815 限制问题
+        original_max_tokens = getattr(self.gpt.sampling_params, 'max_tokens', 'unknown')
+        self.gpt.sampling_params.max_tokens = 3000
+        print(f">> max_tokens 从 {original_max_tokens} 修改为 {self.gpt.sampling_params.max_tokens}")
+
+
         # if self.is_fp16:
         #     self.gpt.eval().half()
         # else:
